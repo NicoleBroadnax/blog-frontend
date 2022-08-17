171056d50a3df861fa9bdd2a930b0176bc4182ad
_NBX_ <104787483+NicoleBroadnax@users.noreply.github.com>
2022-08-17 19:16:50 -0400

    Update APIUrl.js

--- src/APIUrl.js
+++ src/APIUrl.js
@@ -1,3 +1,8 @@
-const APIUrl = "http://localhost:3001";
+let APIUrl = "http://localhost:3001";
+
+// handle if we're running on heroku
+if (window.location.host.indexOf(".herokuapp.com") !== -1) {
+  APIUrl = "https://nicolebroadnax-blog-backend.herokuapp.com/";
+}
 
 export default APIUrl;
